942b11e93e451ac870ba9276f5d1085d24df08d3
Vince <tavince08@gmail.com>
2023-06-10 09:01:29 +0200

    ms login kinda works

--- app/app.py
+++ app/app.py
@@ -1,6 +1,81 @@
-from flask import Flask, jsonify, request
+## Imports
+from flask import Flask, redirect, request, session, url_for, jsonify, render_template
+from dotenv import load_dotenv
+from flask_session import Session
+import identity.web
+import logging as l
+import os
 
+## Load config
+load_dotenv()
+
+
+## Basic configuration
+l.basicConfig(level=l.INFO)
+
+app_config = {
+    "CLIENT_ID": os.getenv("MS_CLIENT_ID"),
+    "CLIENT_SECRET": os.getenv("MS_CLIENT_SECRET"),
+    "AUTHORITY": os.getenv("MS_AUTHORITY", "https://login.microsoftonline.com/common"),
+    "REDIRECT_PATH": os.getenv("MS_REDIRECT_PATH"),
+    "ENDPOINT": os.getenv("MS_ENDPOINT"),
+    "SCOPE": os.getenv("MS_SCOPE"),
+    "SESSION_TYPE": os.getenv("MS_SESSION_TYPE")
+}
+
+## Check config validity
+required_env_vars = app_config.keys()
+missing_vars = [var for var in required_env_vars if app_config[var] is None]
+if missing_vars:
+    l.error(f"Missing required environment variables: {', '.join(missing_vars)}")
+    exit()
+else:
+    l.info(f"Env vars are valid!")
+
+## Do fucky with the app
 app = Flask(__name__)
+app.config.from_mapping(app_config)
+Session(app)
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+## Auth config
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+## Microsoft auth routes
+@app.route("/mslogin")
+def mslogin():
+    l.info(app_config['REDIRECT_PATH'])
+    scopes = app.config["SCOPE"]
+    if not isinstance(scopes, (list, tuple, set)):
+        scopes = [scopes]
+    return redirect(auth.log_in(scopes=scopes, redirect_uri="http://localhost:5000/msauth_response")["auth_uri"])
+    #return jsonify(auth.log_in(scopes=scopes, redirect_uri=url_for("msauth_response", _external=True)))
+
+@app.route("/msauth_response")
+def msauth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return jsonify({"result": result})
+    return redirect(url_for("msindex"))
+
+@app.route("/mslogout")
+def mslogout():
+    return redirect(auth.log_out(url_for("msindex", _external=True)))
+
+@app.route("/msindex")
+def msindex():
+    if not auth.get_user():
+        return redirect(url_for("mslogin"))
+    return jsonify({"user": auth.get_user()})
+
+# - innentol szar a kod mert presiler irta - #
+
 # Login (POST)
 @app.route('/login', methods=['POST'])
 def login():
